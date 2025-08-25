922d81a0919f9cba95619a182c6c26478e2bd10f
Phitchy <phitchayut.ch@tfac.or.th>
2025-08-25 12:16:54 +0700

    update ts config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false, 
+    output: 'standalone',
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
